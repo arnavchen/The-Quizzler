6025f638dcd09fec4ba41d71ea5f70bc0fe4c1eb
Arnav Chennamaneni <chennamaneni.1@osu.edu>
2025-10-05 20:36:47 -0400

    New day, new dawn, new depression

--- app/src/main/java/com/example/thequizzler/MainActivity.kt
+++ app/src/main/java/com/example/thequizzler/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
@@ -75,7 +76,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NameField(modifier: Modifier = Modifier) {
-    var name = remember { mutableStateOf("") }
+    var name = rememberSaveable { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
         Log.d("Lifecycle", "NameField Composable CREATED")

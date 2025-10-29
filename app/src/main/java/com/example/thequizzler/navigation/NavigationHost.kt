c532deec5dc3e19911b19e732ebe19707b72d96b
Arnav Chennamaneni <chennamaneni.1@osu.edu>
2025-10-29 11:39:44 -0400

    Initial Setup for Compose

--- app/src/main/java/com/example/thequizzler/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/thequizzler/navigation/NavigationHost.kt
@@ -8,13 +8,15 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.thequizzler.ui.screens.home.HomeScreen
+import com.example.thequizzler.ui.screens.sessioninfo.SessionInfoScreen
 import com.example.thequizzler.ui.screens.leaderboard.LeaderboardScreen
 import com.example.thequizzler.ui.screens.sessions.SessionsScreen
 import com.example.thequizzler.ui.screens.settings.SettingsScreen
+import com.example.thequizzler.ui.screens.*
+
 
 @Composable
-fun NavigationHost(navController: NavHostController, modifier: Modifier = Modifier) {
+fun Navigation(navController: NavHostController, modifier: Modifier = Modifier) {
 
     LaunchedEffect(Unit) {
         Log.d("Lifecycle", "Navigation Composable CREATED")
@@ -26,10 +28,39 @@ fun NavigationHost(navController: NavHostController, modifier: Modifier = Modifi
         }
     }
 
-    NavHost(navController = navController, startDestination = Screen.Home.route, modifier = modifier) {
-        composable(Screen.Home.route) { HomeScreen(navController) }
-        composable(Screen.Settings.route) { SettingsScreen(navController) }
-        composable(Screen.Leaderboard.route) { LeaderboardScreen(navController) }
-        composable(Screen.Sessions.route) { SessionsScreen(navController) }
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = modifier
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.Settings.route) {
+            SettingsScreen(navController)
+        }
+
+        composable(Screen.Leaderboard.route) {
+            LeaderboardScreen(navController)
+        }
+
+        composable(Screen.Sessions.route) {
+            SessionsScreen(navController)
+        }
+
+        composable(Screen.MockQuiz.route) { backStackEntry ->
+            val playerName = backStackEntry.arguments?.getString("playerName")
+            MockQuizScreen(navController = navController, playerName = playerName)
+        }
+
+        composable(Screen.SessionInfo.route) { backStackEntry ->
+            val sessionId = backStackEntry.arguments?.getString("sessionId")
+            SessionInfoScreen(navController = navController, sessionId = sessionId)
+        }
+
+        composable(Screen.Results.route) { backStackEntry ->
+            val score = backStackEntry.arguments?.getString("score")?.toIntOrNull() ?: 0
+            ResultsScreen(navController = navController, finalScore = score)
+        }
     }
 }
